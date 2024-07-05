d2571ce3839291ac6653514e784b8891dbacc172
Gray Putnam <gputnam@fnal.gov>
2024-07-05 16:04:35 -0500

    Make fit parameter defaults adaptable.

--- LAr10Ana/SiPMGain.py
+++ LAr10Ana/SiPMGain.py
@@ -20,29 +20,29 @@ def SiPMGain(sipm_data):
         N,_ = np.histogram(sipm_data["hit_amp"][i_sipm], bins=bins)
         Ns.append(N)
 
-    # fit parameters
     NFIT = 5
-    
-    mu_p0 = np.array([0.005*(i+1) for i in range(NFIT)])
-    sig_p0 = np.array([0.0005]*NFIT)
-    A_p0 = np.array([1.2e4/2**i for i in range(NFIT)])
-    
-    p0 = sum([[A_p0[i], mu_p0[i], sig_p0[i]] for i in range(NFIT)], [])
-    
-    mu_lo = mu_p0 - 0.0025
-    mu_hi = mu_p0 + 0.0025
-    
-    sig_lo = np.array([0.00025]*NFIT)
-    sig_hi = np.array([0.00100]*NFIT)
-    
-    A_lo = A_p0/2
-    A_hi = A_p0*2
-    
-    lo = sum([[A_lo[i], mu_lo[i], sig_lo[i]] for i in range(NFIT)], [])
-    hi = sum([[A_hi[i], mu_hi[i], sig_hi[i]] for i in range(NFIT)], [])
-
     gains = []
     for i_sipm in range(len(Ns)):
+        # fit parameters
+        mu_p0 = np.array([0.005*(i+1) for i in range(NFIT)])
+        sig_p0 = np.array([0.0005]*NFIT)
+        A_p0 = np.array([np.maximum(Ns[i_sipm])/1.5**i for i in range(NFIT)])
+        
+        p0 = sum([[A_p0[i], mu_p0[i], sig_p0[i]] for i in range(NFIT)], [])
+        
+        mu_lo = mu_p0 - 0.0025
+        mu_hi = mu_p0 + 0.0025
+        
+        sig_lo = np.array([0.00025]*NFIT)
+        sig_hi = np.array([0.00100]*NFIT)
+        
+        A_lo = A_p0/2
+        A_hi = A_p0*2
+        
+        lo = sum([[A_lo[i], mu_lo[i], sig_lo[i]] for i in range(NFIT)], [])
+        hi = sum([[A_hi[i], mu_hi[i], sig_hi[i]] for i in range(NFIT)], [])
+
+        # Do the fit
         where_fit = (centers > 0.003) & (centers < 0.027)
         popt, perr = curve_fit(ngauss, centers[where_fit], Ns[i_sipm][where_fit], p0=p0, bounds=(lo, hi))
         
