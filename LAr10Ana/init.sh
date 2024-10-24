6c0c6326a3ce8975d31682dceb0b40dff27b0bab
Gray Putnam <gputnam@fnal.gov>
2024-10-24 16:49:40 -0500

    Make init/setup scripts runnable from anywhere.

--- LAr10Ana/init.sh
+++ LAr10Ana/init.sh
@@ -1,5 +1,6 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 unset PYTHONHOME
-python3 -m venv env
-source env/bin/activate
+python3 -m venv $SCRIPT_DIR/env
+source $SCRIPT_DIR/env/bin/activate
 pip install --upgrade pip
-pip install -r ${1}requirements.txt
+pip install -r $SCRIPT_DIR/${1}requirements.txt
--- LAr10Ana/setup.sh
+++ LAr10Ana/setup.sh
@@ -1,3 +1,4 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 unset PYTHONHOME
-. env/bin/activate
-export PYTHONPATH=$PYTHONPATH:$PWD
+. $SCRIPT_DIR/env/bin/activate
+export PYTHONPATH=$PYTHONPATH:$SCRIPT_DIR/..
